47a2e87b7de125697cd7c87035939a6e14e1a39a
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-08-04 14:18:10 +0200

    zadanie39476js-tablica-ze-zmiennymi

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ let firstName="Piotr";
 let lastName="Kaczmarzyk";
 console.log("Uwielbiam JavaScript!");
 let array =[1,2,3];
+let liczba1 =5;
+let liczba2 =5;
+let liczba3 =5;
+let array1=[liczba1,liczba2,liczba3];
