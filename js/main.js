7603df5bbbca05036fc6f371840842acfb82be4a
trismagostis <piotr.kaczmarzyk@poczta.fm>
2022-08-04 14:34:15 +0200

    zadanie39478js-warunki

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ let liczba2 =5;
 let liczba3 =5;
 let array1=[liczba1,liczba2,liczba3];
 alert("Skrypt działa prawidłowo!");
+let liczba=12;
+if (liczba>10) {
+ console.log("Podana liczba jest większa od 10");   
+}else {
+    console.log("Podana liczba nie jest większa od 10");
+}
